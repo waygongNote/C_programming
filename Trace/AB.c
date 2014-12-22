0a79646ff9bce585a92a442ef2168795f1e95c90
U-wgtr-pc\waygong <waygongnote@gmail.com>
2014-12-23 00:07:43 +0800

    a ci

--- Trace/AB.c
+++ Trace/AB.c
@@ -6,6 +6,10 @@ __attribute__((no_instrument_function))
     {
 		printf("Enter\n");
 		printf("*func = %p, *callsite = %p\n", func, callsite);
+		system("test -e \"test.map\" &&\
+				cat \"test.map\" |grep \"\ main$\" |\
+				sed \"'\"s/^[ ]*//g\"'\" |awk \"'\"{FS=\" \"}{print $2}\"'\"exit -1 ||\
+				echo \"test.map\"\" doesn\"'\"t exist\"");
     }
 __attribute__((no_instrument_function))
 	void __cyg_profile_func_exit(void *func_address, void *call_site){
@@ -23,3 +27,12 @@ int main(int argc, char *argv[]) {
 }
 // add "-finstrument-functions" when compiler!!
 // maybe dladdr() or backtrace() are ok, too. => But I don't know how to use them.
+//gcc -Wl,-Map=test.map -g -o test test.c
+//cat test.map |grep "\ main$" |sed 's/^[ ]*//g' |awk '{FS=" "}{print $2}'
+
+
+//if [ $# == 1 ] && [ $(echo $0 | tail -c 14) == "if_else_fi.sh" ]; then
+//    echo "$@"
+//	echo "$(echo $0 | tail -c 14)"
+//fi
+
